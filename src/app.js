e9735b604f9791e2ff83d42c470424503927e672
Marvki <marvkipi@gmail.com>
2016-06-05 20:39:13 -0700

    fix

--- src/app.js
+++ src/app.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import styles from './styles.module.css'
-
 const App = React.createClass({
   render: function() {
     return (
-      <div className={styles['container']}>
+      <div>
         Text text text
       </div>
     );
